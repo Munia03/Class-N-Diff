e897c36296e0ccd9c7fd5868179d56a3e8896dba
Nusrat Munia <37515687+Munia03@users.noreply.github.com>
2025-12-05 00:43:37 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python generation_text2img.py --model DiT-L/4 --image-size 256  --ckpt results/044-DiT-L-4/checkpoints/0100000.pt
+python generation.py --model DiT-L/4 --image-size 256  --ckpt results/044-DiT-L-4/checkpoints/0100000.pt
